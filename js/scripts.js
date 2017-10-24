d73a6c691173795ce4284e6c4a793560983273d6
amy churchwell and luke bertram <amy@>
2017-10-24 16:39:41 -0700

    Add html form and javascript logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
+// accepts string, returns true if string == palindrome, false otherwise
+var isPalindrome = function (string){
+  
+  var stringArray = string.split("");
+  stringArray = stringArray.reverse();
+  var reverse = stringArray.join("");
+  return (string === reverse);
+}
 
 $(document).ready(function(){
+  $("form#machine").submit(function(event){
+    $("#result").text("");
+    event.preventDefault();
+    var potentialDrome = $("#input").val();
 
+    var result = isPalindrome(potentialDrome);
+
+    if (result === true) {
+      $("#result").append("That's a real deal palindrome!");
+    } else {
+      $("#result").append("That's a non-palindromic disaster!")
+    }
+  });
 });
